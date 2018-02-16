b31e02bfc67bfdce532eee0dcefdfc90f29017d1
Roman Kovtun <roman.kovtun@webtomed.com>
2018-02-16 17:14:21 -0600

    creating state to set current dice

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let scores, roundScores, activePlayer, dice;
+
+scores = [0, 0];
+roundScores = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+document.querySelector('#current-0').textContent = dice;

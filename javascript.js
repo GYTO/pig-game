d2a1bf87ef775a95138749fd3229914c2e3324d3
Roman Kovtun <roman.kovtun@webtomed.com>
2018-02-19 10:50:42 -0600

    updating the project with new styles and adding dice to the user view

--- javascript.js
+++ javascript.js
@@ -1,10 +1,27 @@
-let scores, roundScores, activePlayer, dice;
+let scores, roundScores, activePlayer;
 
 scores = [0, 0];
 roundScores = 0;
-activePlayer = 0;
+activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-document.querySelector('#current-0').textContent = dice;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+let x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+
+document.querySelector('.dice').style.display = 'none';
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + "</em>";
+
+
+document.querySelector('.btm-roll').addEventListener('click', function () {
+
+  // 1. Random Button
+  let dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display Result
+  document.querySelector('.dice').style.display = 'block';
+
+  // 3.Update the round score IF the rolled number was not a 1
+});

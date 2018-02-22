422b839753ebad4a5c9be1947bf8f2f12873fb69
Roman Kovtun <roman.kovtun@webtomed.com>
2018-02-22 17:03:07 -0600

    creating new type of animation to cover the game rules

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-// const questionButtons = document.getElementsByClassName('btn-flip');
-//
-// for (let i = 0; i < questionButtons.length; i++){
-//   questionButtons[i].addEventListener('click', function () {
-//     document.querySelector('.card_wrapper').classList.toggle('flipped');
-//   })
-// }
+const questionButtons = document.getElementsByClassName('btn-flip');
+
+let height = 0,
+    width = 0,
+    radius = 0;
+
+setSize();
+
+
+function setSize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    radius = Math.sqrt(width * width + height * height);
+}
+
+window.addEventListener('resize', function () {setSize()});
+
+for (let i = 0; i < questionButtons.length; i++){
+  questionButtons[i].addEventListener('click', function (element) {
+
+    let circle = textContent('<div unselectable="on" id="circle"></div>')
+    // reference https://codemyui.com/material-design-button-colour-background-colour/
+  })
+}
+
+

6179174f9650402ed34d8e71dbeeac5a7f7ffb11
Roman Kovtun <roman.kovtun@webtomed.com>
2018-02-22 16:33:21 -0600

    implement mobile version of the app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const questionButtons = document.getElementsByClassName('btn-flip');
-
-for (let i = 0; i < questionButtons.length; i++){
-  questionButtons[i].addEventListener('click', function () {
-    document.querySelector('.card_wrapper').classList.toggle('flipped');
-  })
-}
+// const questionButtons = document.getElementsByClassName('btn-flip');
+//
+// for (let i = 0; i < questionButtons.length; i++){
+//   questionButtons[i].addEventListener('click', function () {
+//     document.querySelector('.card_wrapper').classList.toggle('flipped');
+//   })
+// }

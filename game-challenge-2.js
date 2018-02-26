a0ef3c6ade90b5ea191e3ce76c214f355d4de5c2
Roman Kovtun <roman.kovtun@webtomed.com>
2018-02-26 10:23:44 -0600

    build second type of the game

---
+++ game-challenge-2.js
@@ -0,0 +1,128 @@
+let scores, roundScores, activePlayer, gamePlaying;
+
+let scoreZero = document.getElementById('score-0'),
+    scoreOne = document.getElementById('score-1');
+
+let currentZero = document.getElementById('current-0'),
+    currentOne = document.getElementById('current-1');
+
+let dice = document.querySelector('.dice');
+
+let playerZeroPanel = document.querySelector('.player-0-panel'),
+    playerOnePanel = document.querySelector('.player-1-panel');
+
+init();
+
+let lastDice;
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    if (gamePlaying) {
+        // 1. Random Button
+        let dice = Math.floor(Math.random() * 6) + 1;
+        // 2. Display Result
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'images/dice-' + dice + '.png';
+
+        // 3.Update the round score IF the rolled number was not a 1
+
+        if (dice === 6 && lastDice === 6) {
+
+            // Player loose the score
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            nextPlayer();
+        } else if (dice !== 1) {
+            // add score
+            roundScores += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScores;
+        } else {
+            // next player
+            nextPlayer();
+        }
+
+        lastDice = dice;
+    }
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+
+    if (gamePlaying) {
+        //add current score to global score
+        scores[activePlayer] += roundScores;
+        //update UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+
+        let input = document.querySelector('.final-score').value;
+        let winningScore;
+
+        // undefined, 0, null, or "" are Coerced to false
+        // anything else is coerced to true
+        if(input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+
+        //check if player won the game
+
+        if (scores[activePlayer] >= winningScore) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            dice.style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+
+            document.getElementById('canvas').style.display = 'block';
+            firework();
+
+        } else {
+            // next player
+            nextPlayer();
+        }
+    }
+
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScores = 0;
+
+    currentZero.textContent = '0';
+    currentOne.textContent = '0';
+    playerZeroPanel.classList.toggle('active');
+    playerOnePanel.classList.toggle('active');
+
+    dice.style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScores = 0;
+
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    scoreZero.textContent = '0';
+    scoreOne.textContent = '0';
+    currentZero.textContent = '0';
+    currentOne.textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    playerZeroPanel.classList.remove('winner');
+    playerOnePanel.classList.remove('winner');
+    playerZeroPanel.classList.remove('active');
+    playerOnePanel.classList.remove('active');
+
+    playerZeroPanel.classList.add('active');
+
+    document.getElementById('canvas').style.display = 'none';
+
+}
